dfd0a946cd3bd93861dd936a68cb2926639619a4
Abraão Moreira <abraaol.moreira@gmail.com>
2023-07-26 00:31:34 -0300

    configure Elsa

--- ElsaStudies.Server.Dashboard/Program.cs
+++ ElsaStudies.Server.Dashboard/Program.cs
@@ -1,16 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var elsaSection = builder.Configuration.GetSection("Elsa");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddElsa(elsa => elsa
+    .AddHttpActivities()
+    .AddEmailActivities(elsaSection.GetSection("Smtp").Bind))
+    .AddElsaApiEndpoints();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -19,6 +26,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseHttpActivities();
 
 app.MapControllerRoute(
     name: "default",
